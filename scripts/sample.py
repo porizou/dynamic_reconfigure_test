7fe32c0fab3aa0a8875bb5f5a2a5ef546d241f22
porizou <porizou.electro@gmail.com>
2021-01-01 18:58:23 +0900

    first commit

---
+++ scripts/sample.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rospy
+import dynamic_reconfigure.client
+
+if __name__ == '__main__':
+    rospy.init_node("dynamic_reconfigure_client")
+    client = dynamic_reconfigure.client.Client("/move_base/local_costmap/inflation_layer/")
+    while not rospy.is_shutdown():
+        client.update_configuration({"inflation_radius": 0.5})
+        rospy.sleep(1)
+        client.update_configuration({"inflation_radius": 1.0})
+        rospy.sleep(1)
+        
+    rospy.spin()
